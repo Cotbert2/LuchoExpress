64b4b70487364914629d41aa64e8b7f4e0840054
Cotbert2 <mateojgdev@gmail.com>
2025-08-02 11:09:42 -0500

    order microservice

---
+++ backend/ms-order/src/main/java/com/bitcrack/luchoexpress/LuchoExpressOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bitcrack.luchoexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuchoExpressOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuchoExpressOrderServiceApplication.class, args);
+	}
+
+}
