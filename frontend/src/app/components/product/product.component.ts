87666ac805060b4b69b3aa03a2d46c30e4a8cc54
Cotbert2 <mateojgdev@gmail.com>
2025-08-02 21:52:30 -0500

    product

--- frontend/src/app/components/product/product.component.ts
+++ frontend/src/app/components/product/product.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
+import { ImageModule } from 'primeng/image';
+import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [
+    CommonModule, 
+    InputNumberModule, 
+    ButtonModule, 
+    ImageModule,
+    PanelModule,
+    TableModule,
+    RatingModule,
+    FormsModule
+  ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  @Input() product: any;
+  @Output() addToCartEvent = new EventEmitter<any>();
+
+  
+  tableData : any = [];
+  productDescription : any = {};
+  cuantity : number = 1;
+
+
+  rating: number = 3;
+
+  ngOnInit(): void {
+    this.rating = this.generateRandomNumber();
+    console.log('product: ',this.product);
+    this.productDescription = JSON.parse(this.product.description);
+    console.log('productDescription: ',this.productDescription);
+    this.tableData = Object.entries(this.productDescription).map(([key, value]) => ({ key, value }));
+  }
+
+  addToCart() : void{
+    const itemToCard = {
+      product: this.product,
+      cuantity: this.cuantity
+    };
+    console.log('new ited selected',itemToCard);
+    this.addToCartEvent.emit(itemToCard);
+  }
+
+
+  generateRandomNumber(): number {
+    return Math.floor(Math.random() * (5 - 3 + 1)) + 3;
+  }
 
 }
