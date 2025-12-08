8b2d407da90f45e76c2a3e9a15c76de861e532c5
Cotbert2 <mtaeojgdev@gmail.com>
2025-12-08 18:00:00 -0500

    cart badge

--- lucho-express-mobile/src/app/app.component.ts
+++ lucho-express-mobile/src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonApp, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonBadge } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { homeOutline, bagOutline, cubeOutline, cartOutline, personOutline } from 'ionicons/icons';
+import { CartService } from './services/cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -19,10 +21,24 @@ import { homeOutline, bagOutline, cubeOutline, cartOutline, personOutline } from
     IonBadge
   ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   cartItemsCount = 0;
+  private cartSubscription?: Subscription;
 
-  constructor() {
+  constructor(private cartService: CartService) {
     addIcons({ homeOutline, bagOutline, cubeOutline, cartOutline, personOutline });
+  }
+
+  ngOnInit(): void {
+    // Subscribe to cart changes
+    this.cartSubscription = this.cartService.cartItems$.subscribe(items => {
+      this.cartItemsCount = items.reduce((total, item) => total + item.quantity, 0);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.cartSubscription) {
+      this.cartSubscription.unsubscribe();
+    }
   }
 }
