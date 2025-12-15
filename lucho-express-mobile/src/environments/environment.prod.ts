4cc7faa53d1930125a928b3c82070e456673d793
Cotbert2 <mtaeojgdev@gmail.com>
2025-12-15 03:14:28 -0500

    changes

--- lucho-express-mobile/src/environments/environment.prod.ts
+++ lucho-express-mobile/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://10.9.7.221.com',
-  authUrl: 'https://10.9.7.221.com/auth',
-  productsUrl: 'https://10.9.7.221.com:8085/api',
-  customersUrl: 'https://10.9.7.221.com:8082',
-  ordersUrl: 'https://10.9.7.221.com:8084/api',
-  trackingApiUrl: 'https://10.9.7.221.com:8086',
-  chatSocketUrl: 'https://10.9.7.221:3000'  // Direct connection to ms-chat for WebSocket
+  apiUrl: 'https://10.40.15.116.com',
+  authUrl: 'https://10.40.15.116.com/auth',
+  productsUrl: 'https://10.40.15.116.com:8085/api',
+  customersUrl: 'https://10.40.15.116.com:8082',
+  ordersUrl: 'https://10.40.15.116.com:8084/api',
+  trackingApiUrl: 'https://10.40.15.116.com:8086',
+  chatSocketUrl: 'https://10.40.15.116:3000'  // Direct connection to ms-chat for WebSocket
 };
--- lucho-express-mobile/src/environments/environment.ts
+++ lucho-express-mobile/src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://10.9.7.221:8080',
-  authUrl: 'http://10.9.7.221:8080/auth',
-  productsUrl: 'http://10.9.7.221:8080/api',
-  customersUrl: 'http://10.9.7.221:8080',
-  ordersUrl: 'http://10.9.7.221:8080/api',
-  trackingApiUrl: 'http://10.9.7.221:8080',
-  chatSocketUrl: 'http://10.9.7.221:3000'  // Direct connection to ms-chat for WebSocket
+  apiUrl: 'http://10.40.15.116:8080',
+  authUrl: 'http://10.40.15.116:8080/auth',
+  productsUrl: 'http://10.40.15.116:8080/api',
+  customersUrl: 'http://10.40.15.116:8080',
+  ordersUrl: 'http://10.40.15.116:8080/api',
+  trackingApiUrl: 'http://10.40.15.116:8080',
+  chatSocketUrl: 'http://10.40.15.116:3000'  // Direct connection to ms-chat for WebSocket
 };
