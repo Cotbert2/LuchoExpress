6c0a7ab85471355fcd67d1f2f7d200f97d16585c
Cotbert2 <mtaeojgdev@gmail.com>
2025-12-15 20:53:45 -0500

    consume dns from app

--- lucho-express-mobile/src/environments/environment.prod.ts
+++ lucho-express-mobile/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://10.40.15.116.com',
-  authUrl: 'https://10.40.15.116.com/auth',
-  productsUrl: 'https://10.40.15.116.com:8085/api',
-  customersUrl: 'https://10.40.15.116.com:8082',
-  ordersUrl: 'https://10.40.15.116.com:8084/api',
-  trackingApiUrl: 'https://10.40.15.116.com:8086',
-  chatSocketUrl: 'https://10.40.15.116:3000'  // Direct connection to ms-chat for WebSocket
+  apiUrl: 'https://luchoexpress.site.com',
+  authUrl: 'https://luchoexpress.site.com/auth',
+  productsUrl: 'https://luchoexpress.site.com:8085/api',
+  customersUrl: 'https://luchoexpress.site.com:8082',
+  ordersUrl: 'https://luchoexpress.site.com:8084/api',
+  trackingApiUrl: 'https://luchoexpress.site.com:8086',
+  chatSocketUrl: 'https://luchoexpress.site:3000'  // Direct connection to ms-chat for WebSocket
 };
--- lucho-express-mobile/src/environments/environment.ts
+++ lucho-express-mobile/src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://10.40.15.116:8080',
-  authUrl: 'http://10.40.15.116:8080/auth',
-  productsUrl: 'http://10.40.15.116:8080/api',
-  customersUrl: 'http://10.40.15.116:8080',
-  ordersUrl: 'http://10.40.15.116:8080/api',
-  trackingApiUrl: 'http://10.40.15.116:8080',
-  chatSocketUrl: 'http://10.40.15.116:3000'  // Direct connection to ms-chat for WebSocket
+  apiUrl: 'http://luchoexpress.site:8080',
+  authUrl: 'http://luchoexpress.site:8080/auth',
+  productsUrl: 'http://luchoexpress.site:8080/api',
+  customersUrl: 'http://luchoexpress.site:8080',
+  ordersUrl: 'http://luchoexpress.site:8080/api',
+  trackingApiUrl: 'http://luchoexpress.site:8080',
+  chatSocketUrl: 'http://luchoexpress.site:3000'  // Direct connection to ms-chat for WebSocket
 };
